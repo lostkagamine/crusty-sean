8745ac261954471c2b65a4c115696bb4d83f1397
Luna <ry00001@protonmail.com>
2022-09-18 19:05:21 +0100

    wow i suck at programming dont i

--- splatoonbot/src/main.rs
+++ splatoonbot/src/main.rs
@@ -106,7 +106,8 @@ async fn main() -> Result<(), anyhow::Error> {
 
     let mut scheds = api::get_latest_schedules().await;
 
-    if std::env::args().collect::<Vec<String>>()[1] == "force" {
+    let argv = std::env::args().collect::<Vec<String>>();
+    if argv.len() >= 2 && argv[1] == "force" {
         println!("forcing post");
         mastodon.make_post(&get_sched_text(&scheds)).await;
         return Ok(())
